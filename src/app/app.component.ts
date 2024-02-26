55f3c313ba32dec2838a5b84f55614a747d22e3e
ahmed kassem <jeeeko@outlook.com>
2024-02-26 02:13:14 +0200

    13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MobileSdkMessage } from './mobile-sdk-message';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @HostListener('window:message', ['$event'])
+  onWindowMessage(event: MessageEvent) {
+    debugger;
+    try {
+      console.dir(event);
+      const message: MobileSdkMessage = event.data as MobileSdkMessage;
+      if (this.isValidMessage(message)) {
+        debugger;
+        console.log('Received message from window:', message.html);
+      }
+    } catch (error) {
+      console.error('Error handling window message:', error);
+    }
+  }
+  isValidMessage(message: any) {
+    return (
+      typeof message === 'object' &&
+      message !== null &&
+      'html' in message &&
+      typeof message.html === 'string'
+      // Add additional checks for other properties if needed
+    );
+  }
   title = 'jsChannel';
+  clicked(){
+    window.postMessage({html:"test"});
+  }
 }
---
+++ src/app/mobile-sdk-message.ts
@@ -0,0 +1,3 @@
+export interface MobileSdkMessage {
+    html:string;
+}
